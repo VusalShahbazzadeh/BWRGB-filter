5e0ee95f99fe3493333bd95b9eec99c3db62b959
Vusal <vusal.shahbazzadeh@gmail.com>
2020-03-01 16:56:54 +0400

    Wrote BWCMY.py

---
+++ BWCMY.py
@@ -0,0 +1,34 @@
+from PIL import Image
+
+img = Image.open('Example.png')
+size = img.size
+pixels = img.load()
+
+for i in range(size[1]):
+    for j in range(size[0]):
+        if (len(pixels[0, 0]) == 4):
+            (R, G, B, A) = img.getpixel((j, i))
+            MMM = list((R, G, B))
+            MMM.sort()
+            T = MMM[1]
+            if (G == MMM[2]):
+                pixels[j, i] = (R, T, B, A)
+            elif (R == MMM[2]):
+                pixels[j, i] = (T, G, B, A)
+            elif (B == MMM[2]):
+                pixels[j, i] = (R, G, T, A)
+        elif (len(pixels[0, 0]) == 3):
+            (R, G, B) = img.getpixel((j, i))
+            MMM = list((R, G, B))
+            MMM.sort()
+            T = MMM[1]
+            if (G == MMM[2]):
+                pixels[j, i] = (R, T, B)
+            elif (R == MMM[2]):
+                pixels[j, i] = (T, G, B)
+            elif (B == MMM[2]):
+                pixels[j, i] = (R, G, T)
+
+
+img.show()
+img.save('ModifiedExample.png')
