4d061f0efd4569c1bd389be3ca10f0af60d684c7
Vusal <vusal.shahbazzadeh@gmail.com>
2020-07-14 11:21:47 +0400

    Added Auto Color Detection for CC filter
    Used K-means algorithm to find colors for CC filter

--- CC.py
+++ CC.py
@@ -1,16 +1,50 @@
 from PIL import Image
 from math import sqrt
+import numpy as np
 
 img = Image.open('Example.png')
 size = img.size
 pixels = img.load()
 
+# Modify only below this
 brightness_scaler = 1
+
 colors = [
     (255, 0, 0),
-    (255, 255, 0),
-    (0, 0, 0)
+    (0, 0, 0),
+    (255, 255, 0)
 ]
+
+use_auto_color_choice = False
+number_of_colors = 7
+# Modify only above this
+if use_auto_color_choice:
+    pixel_array = []
+
+    for i in range(size[1]):
+        for j in range(size[0]):
+            pixel_array.append(pixels[j, i])
+
+    pixel_array = np.asarray(pixel_array)
+
+    from sklearn.cluster import KMeans
+
+    classifier = KMeans(n_clusters=number_of_colors, init='k-means++')
+    classifier.fit(pixel_array)
+
+    centers = classifier.cluster_centers_
+
+
+    def array3_to_tuple_int(array):
+        return int(array[0]), int(array[1]), int(array[2])
+
+
+    tuple_centers = []
+
+    for i in range(len(centers)):
+        tuple_centers.append(array3_to_tuple_int(centers[i]))
+
+    colors = tuple_centers
 
 
 def dis(c1, c2):
